66cc0b9bca3af0134abf53b98895237f6111ad90
roberto <robertocd509@gmail.com>
2023-10-11 11:08:52 +0200

    cambios dia 11/10

--- src/Ej5.java
+++ src/Ej5.java
@@ -5,16 +5,48 @@ public class Ej5 {
         Scanner teclado = new Scanner(System.in);
         int numeros;
         int contadorPositivos = 0;
+        int sumaPositivos = 0;
+        int numeroMinimo = Integer.MAX_VALUE;
+        int numeroMaximo = Integer.MIN_VALUE;
 
         do {
-            System.out.println("Introduce numeros (negativos para terminar)");
+            System.out.println("Introduce números (negativos para terminar)");
             numeros = teclado.nextInt();
 
             if (numeros > 0) {
+                sumaPositivos += numeros;
                 contadorPositivos++;
+                if (numeros < numeroMinimo) {
+                    numeroMinimo = numeros;
+                }
+                if (numeros > numeroMaximo) {
+                    numeroMaximo = numeros;
+                }
             }
         } while (numeros > 0);
 
-        System.out.println("Se han introducido " + contadorPositivos + " números positivos (incluyendo 0).");
+        if (contadorPositivos > 0) {
+            double media = (double) sumaPositivos / contadorPositivos;
+            
+            System.out.println("Se han introducido " + contadorPositivos + " números positivos (incluyendo 0).");
+            System.out.println("La media de los números positivos es: " + media);
+            System.out.println("El número mínimo es: " + numeroMinimo);
+            System.out.println("El número máximo es: " + numeroMaximo);
+
+            // Ahora, solicitemos los números mayores que la media
+            System.out.println("Números superiores a la media:");
+            numeros = teclado.nextInt();
+
+            do {
+                System.out.println("Introduce un número (negativo para terminar)");
+                numeros = teclado.nextInt();
+
+                if (numeros > media) {
+                    System.out.println(numeros + " es mayor que la media.");
+                }
+            } while (numeros > 0);
+        } else {
+            System.out.println("No se introdujeron números positivos.");
+        }
     }
 }
